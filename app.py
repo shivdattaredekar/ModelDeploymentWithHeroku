4bf2ee2d69cef408db35ef89969dfec2b3d9be63
Shivdatta Redekar <46707992+shivdattaredekar@users.noreply.github.com>
2024-03-19 13:04:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET'])
 def predict():
     '''
     For rendering results on HTML GUI
